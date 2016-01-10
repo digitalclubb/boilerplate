1d745008b611181f96dc40e38c1aa6ae820b432e
Gareth Clubb <thewelsh@gmail.com>
2016-01-10 12:01:29 +0000

    Add jQuery and gulp concat

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 
 var gulp = require( 'gulp' );
 var sass = require( 'gulp-sass' );
+var concat = require('gulp-concat');
 
 gulp.task( 'default', function() {
 
@@ -11,4 +12,11 @@ gulp.task('sass', function () {
 	gulp.src( './sass/**/*.scss' )
 		.pipe( sass( { outputStyle: 'compressed' } ) )
 		.pipe( gulp.dest( './deploy/css' ) );
+});
+
+ 
+gulp.task('js', function() {
+	return gulp.src('./js/*.js')
+		.pipe(concat('main.js'))
+		.pipe(gulp.dest('./deploy/js'));
 });
